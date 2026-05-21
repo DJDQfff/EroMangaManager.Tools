f03c99cd3b62d6f099d160e43b6f04995bb57a7e
changbao zuo <2743109998@QQ.com>
2026-05-21 15:46:26 +0800

    添加步骤，三个msix移到publish文件夹，并创建msixbundle

--- PublishAllCommand.cs
+++ PublishAllCommand.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+using System;
 using System.Diagnostics;
 using System.Runtime.InteropServices;
 using System.Security.Cryptography;
@@ -8,14 +10,19 @@ public class DotnetMakePackages
 
     public  void Run ()
     {
+        var packtool = Directory.GetDirectories("C:\\Program Files (x86)\\Microsoft Visual Studio\\Shared\\NuGetPackages\\microsoft.windows.sdk.buildtools\\" , "x64" , new EnumerationOptions() { RecurseSubdirectories = true }).First();
+        var makeappx = Directory.GetFiles(packtool).Single( x => x.EndsWith("makeappx.exe"));
+        var signtool = Directory.GetFiles(packtool).Single(x => x.EndsWith("signtool.exe"));
+
         string rootPath = "E:\\Projects\\EroMangaManager";
         string winappcsproj = "E:\\Projects\\EroMangaManager\\WinApp\\WinApp.csproj";
         string unoappcsproj = "E:\\Projects\\EroMangaManager\\UnoApp\\UnoApp\\UnoApp.csproj";
         string binPath_WinApp= "E:\\Projects\\EroMangaManager/WinApp/bin";
         string unorelease = "E:\\Projects\\EroMangaManager\\UnoApp\\UnoApp\\bin\\Release\\net10.0-android";
-        string publishDirectory = "E:\\Projects\\EroMangaManager\\publish";
+        var publishDirectory = new  DirectoryInfo("E:\\Projects\\EroMangaManager\\publish");
         string pfxPath = "E:\\Projects\\EroMangaManager\\DJDQfff_new.pfx";
         string pfxPassword = Environment.GetEnvironmentVariable("MADAO_PASSWORD");
+        string msixbundleFIle = "E:\\Projects\\EroMangaManager\\publish\\upload.msixbundle";
         var platforms = new string[] { "x64" , "x86" , "ARM64" };
         var runtimeidentifiers_android = new string[] { "android-arm64" , "android-arm" };
         var rids = new Dictionary<string , string>
@@ -30,15 +37,27 @@ public class DotnetMakePackages
 
             var tempDirectory = Directory.CreateTempSubdirectory().FullName;
 
-            Process.Start("dotnet" , $" build {winappcsproj} -c Release -p:Platform={platform} -p:RuntimeIdentifier={rids[platform]} -p:AppxBundle=Never /p:GenerateAppxPackageOnBuild=true /p:PackageCertificateKeyFile=\"DJDQfff_new.pfx\" /p:PackageCertificatePassword=512131415zcb").WaitForExit();
+            Process.Start("dotnet" , $" build {winappcsproj} -c Release -p:Platform={platform} -p:RuntimeIdentifier={rids[platform]} -p:AppxBundle=Never /p:GenerateAppxPackageOnBuild=true /p:PackageCertificateKeyFile={pfxPath} /p:PackageCertificatePassword={pfxPassword}").WaitForExit();
 
             var folder = Path.Combine(binPath_WinApp , platform);
             folder = Path.Combine(folder , "Release");
             var files = Directory.EnumerateFiles(folder , $"*{platform}.msix" , new EnumerationOptions() { RecurseSubdirectories = true });
             var msix = files.Single();
+            var target = Path.Combine(publishDirectory.FullName , $"{Path.GetFileName(msix)}");
+            File.Copy(msix , target,true);
+
             Files.Add(msix);
         }
-        foreach(var runtime in runtimeidentifiers_android)
+
+        Process.Start(makeappx, $"bundle /o /d \"{publishDirectory.FullName}\" /p \"{msixbundleFIle}\"").WaitForExit();
+
+        
+        Process
+            .Start(signtool , $"sign /fd SHA256 /a /f \"{pfxPath}\" /p {pfxPassword}  \"{msixbundleFIle}\"")
+            .WaitForExit();
+
+
+        foreach (var runtime in runtimeidentifiers_android)
         {
             var tempDirectory = Directory.CreateTempSubdirectory().FullName;
 
