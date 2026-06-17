b6a129af75c918d2454112eb8700856439307efa
changbao zuo <2743109998@QQ.com>
2026-06-17 12:21:42 +0800

    v2026.6.16

--- Program.cs
+++ Program.cs
@@ -4,21 +4,20 @@ using Tools;
 
 Console.WriteLine("Hello, World!");
 
-var version = "2026.6.15";
+var version = "2026.6.16";
 var slnfolder = "E:\\Projects\\EroMangaManager";
-var publishversion = Path.Combine(slnfolder , "publish" , slnfolder);
-var files = Directory.EnumerateFiles(publishversion);
-
-
-DotnetMakePackages maker = new(version ,slnfolder);
-
-maker.CleanBuildDirectories();
-maker.BuildMsix();
-maker.PubllishAPK();
 
 var publisher = new GitHubReleasePublisher("DJDQfff" , "EroMangaManager");
 
-await publisher.PublishAsync(version , [.. files]);
+DotnetMakePackages maker = new(version ,slnfolder);
+
+//maker. CleanThenRestoreSlnx();
+
+maker.BuildMsix();
+maker.PubllishAPK();
+
+
+await publisher.PublishAsync(version , maker.Files);
 
 
 
--- PublishAllCommand.cs
+++ PublishAllCommand.cs
@@ -5,58 +5,57 @@ namespace Tools;
 public class DotnetMakePackages
 {
     public List<string> Files { get; } = [];
-    private readonly string rootPath ;
-    private readonly string pfxPath;
-    private readonly string pfxPassword=null!;
-    private readonly string version;
-    private readonly string packtoolfolder;
-    private readonly string makeappx;
-    private readonly string signtool;
-    private readonly string msixbundleFIle;
-    private readonly string publishversionfolder;
-    private readonly string slnPath;
-    public DotnetMakePackages (string _version,string slnFolder)
+    readonly string rootPath;
+    readonly string pfxPath;
+    readonly string PfxPassword = Environment.GetEnvironmentVariable("MADAO_PASSWORD") ?? throw new ArgumentException("环境变量：证书密码未配置");
+
+    readonly string version;
+    readonly string packtoolfolder;
+    readonly string makeappx;
+    readonly string signtool;
+    readonly string msixbundleFIle;
+    readonly string publishversionfolder;
+    readonly string slnPath;
+    string MSBuildExe = @"C:\Program Files\Microsoft Visual Studio\18\Community\MSBuild\Current\Bin\MSBuild.exe";
+    string Configuration = "Release";
+    string AndroidTargetFramwork = "net10.0-android";
+
+    string WindowsTargetFrameWork = "net10.0-windows10.0.26100";
+    string[]? WindowsPlatforms = ["x64" , "x86" , "ARM64"];
+    Dictionary<string , string> WindowsRuntimeIdentifiers = new()
+    {
+        ["x64"] = "win-x64" ,
+        ["x86"] = "win-x86" ,
+        ["ARM64"] = "win-arm64"
+    };
+    readonly string[] AndroidRuntimeIdentifiers = ["android-arm64" , "android-arm" ] ;
+
+    public DotnetMakePackages (string _version , string slnFolder)
     {
         rootPath = slnFolder;
         slnPath = Directory.GetFiles(rootPath , "*.slnx").First();
-        pfxPassword = Environment.GetEnvironmentVariable("MADAO_PASSWORD") ?? throw new ArgumentException("环境变量：证书密码未配置");
 
         // ... 其他原有代码保持不变
         version = _version;
         msixbundleFIle = Path.Combine(rootPath , "publish" , $"{version}.msixbundle");
 
         packtoolfolder = Directory.GetDirectories("C:\\Program Files (x86)\\Microsoft Visual Studio\\Shared\\NuGetPackages\\microsoft.windows.sdk.buildtools\\" , "x64" , new EnumerationOptions() { RecurseSubdirectories = true }).First();
-         pfxPath = Path.Combine(rootPath, "DJDQfff_new.pfx");
+        pfxPath = Path.Combine(rootPath , "DJDQfff_new.pfx");
 
         makeappx = Directory.GetFiles(packtoolfolder).Single(x => x.EndsWith("makeappx.exe"));
         signtool = Directory.GetFiles(packtoolfolder).Single(x => x.EndsWith("signtool.exe"));
 
-         publishversionfolder = Path.Combine(slnFolder,"publish" , version);
+        publishversionfolder = Path.Combine(slnFolder , "publish" , version);
         Directory.CreateDirectory(publishversionfolder);
+        Files.Add(Path.Combine(rootPath , "DJDQfff_certificate.cer"));
 
     }
     public void BuildMsix ()
     {
-        var targetframwork = "net10.0-windows10.0.26100";
 
         string winappcsproj = Path.Combine(rootPath , "WinApp/WinApp.csproj");
         string WinApp_bin = Path.Combine(rootPath , "WinApp/bin");
-        // 👇 1. 在构建任何项目之前，先对整个解决方案执行还原
-        if (!string.IsNullOrEmpty(slnPath))
-        {
-            if (!string.IsNullOrEmpty(slnPath))
-            {
-                Run("dotnet" , $"restore \"{slnPath}\"" , rootPath);
-            }
-        }
-        string[]? platforms = ["x64" , "x86" , "ARM64"];
-        var rids = new Dictionary<string , string>
-        {
-            ["x64"] = "win-x64" ,
-            ["x86"] = "win-x86" ,
-            ["ARM64"] = "win-arm64"
-        };
-        foreach (var platform in platforms)
+        foreach (var platform in WindowsPlatforms)
         {
             //var tempDirectory = Directory.CreateTempSubdirectory().FullName;
             //Process.Start("dotnet" ,
@@ -70,85 +69,101 @@ public class DotnetMakePackages
             //    $" -c Release" +
             //    $" -f {targetframwork} " +
             //    $" -p:Platform={platform} " +
-            //    $" -p:RuntimeIdentifier={rids[platform]}" +
+            //    $" -p:RuntimeIdentifier={WindowsRuntimeIdentifiers[platform]}" +
             //    $" -p:AppxBundle=Never " +
             //    $" /p:GenerateAppxPackageOnBuild=true " +
             //    $" /p:PackageCertificateKeyFile={pfxPath} " +
-            //    $" /p:PackageCertificatePassword={pfxPassword}")
+            //    $" /p:PackageCertificatePassword={PfxPassword}")
             //    .WaitForExit();
+            try
+            {
+                string msbuildArgs = $"{winappcsproj} /t:Publish" +
+                                    $" /p:Configuration={Configuration}" +
+                                    $" /p:Platform={platform}" +
+                                    $" /p:RuntimeIdentifier={WindowsRuntimeIdentifiers[platform]}" +
+                                    $" /p:TargetFramework={WindowsTargetFrameWork}" +
+                                    $" /p:AppxBundle=Never" +
+                                    $" /p:GenerateAppxPackageOnBuild=true" +
+                                    $" /p:PackageCertificateKeyFile={pfxPath}" +
+                                    $" /p:PackageCertificatePassword={PfxPassword}";
 
-            var msbuild = @"C:\Program Files\Microsoft Visual Studio\18\Community\MSBuild\Current\Bin\MSBuild.exe";
-            string msbuildArgs = $"{winappcsproj} /t:Publish" +
-                                             $" /p:Configuration=Release" +
-                                             $" /p:Platform={platform}" +
-                                             $" /p:RuntimeIdentifier={rids[platform]}" +
-                                             $" /p:TargetFramework={targetframwork}" +
-                                             $" /p:AppxBundle=Never" +
-                                             $" /p:GenerateAppxPackageOnBuild=true" +
-                                             $" /p:PackageCertificateKeyFile={pfxPath}" +
-                                             $" /p:PackageCertificatePassword={pfxPassword}";
+                Run(MSBuildExe , msbuildArgs , rootPath);
+                var folder = Path.Combine(WinApp_bin , platform , Configuration , WindowsTargetFrameWork , WindowsRuntimeIdentifiers[platform]);
+                var files = Directory.EnumerateFiles(folder , $"*_{platform}.msix" , new EnumerationOptions() { RecurseSubdirectories = true });
+                var msix = files.Single();
+                var target = Path.Combine(publishversionfolder , $"{Path.GetFileName(msix)}");
+                File.Move(msix , target , true);
 
-            Run(msbuild , msbuildArgs , rootPath);
-            var folder = Path.Combine(WinApp_bin , platform , "Release" , targetframwork , rids[platform]);
-            var files = Directory.EnumerateFiles(folder , $"*_{platform}.msix" , new EnumerationOptions() { RecurseSubdirectories = true });
-            var msix = files.Single();
-            var target = Path.Combine(publishversionfolder , $"{Path.GetFileName(msix)}");
-            File.Move(msix , target , true);
+                Files.Add(target);
 
-            Files.Add(msix);
-      
+            }
+            catch(Exception ex) { Console.WriteLine(ex.Message); }
+
         }
 
         // 👇 3. 打包和签名也换成 Run 方法
         Run(makeappx , $"bundle /o /d \"{publishversionfolder}\" /p \"{msixbundleFIle}\"" , rootPath);
-        Run(signtool , $"sign /fd SHA256 /a /f \"{pfxPath}\" /p {pfxPassword} \"{msixbundleFIle}\"" , rootPath);
+        Run(signtool , $"sign /fd SHA256 /a /f \"{pfxPath}\" /p {PfxPassword} \"{msixbundleFIle}\"" , rootPath);
+        Files.Add(msixbundleFIle);
     }
 
-    
+    public void CleanThenRestoreSlnx ()
+    {
+        if (!string.IsNullOrEmpty(slnPath))
+        {
+            // 1. 先清理
+            Run("dotnet" , $"clean \"{slnPath}\"" , rootPath);
+
+            // 2. 再还原（确保后续构建不会报错）
+            Run("dotnet" , $"restore \"{slnPath}\"" , rootPath);
+        }
+    }
     public void PubllishAPK ()
     {
-        var targetframework = "net10.0-android";
 
         string unoappcsproj = Path.Combine(rootPath , "UnoApp/UnoApp.csproj");
 
-        string unorelease = Path.Combine(rootPath , $"UnoApp/bin/Release/{targetframework}");
-        //// 👇 1. 使用 Run 方法对整个解决方案执行还原
-        //if (!string.IsNullOrEmpty(slnPath))
-        //{
-        //    Run("dotnet" , $"restore \"{slnPath}\" -p:TargetFramework={targetframework}" , rootPath);
-        //}
+        string unorelease = Path.Combine(rootPath , $"UnoApp/bin/{Configuration}/{AndroidTargetFramwork}");
 
-
-        var runtimeidentifiers = new string[] { "android-arm64" , "android-arm" };
-        Files.Add(Path.Combine(rootPath , "DJDQfff_certificate.cer"));
-
-
-        foreach (var runtime in runtimeidentifiers)
+        foreach (var runtime in AndroidRuntimeIdentifiers)
         {
-            //var tempDirectory = Directory.CreateTempSubdirectory().FullName;
-            // 发布 APK
-            Run("dotnet" , $"publish \"{unoappcsproj}\" -f {targetframework} -r {runtime} -c Release" , rootPath);
+            try
+            {
+                //var tempDirectory = Directory.CreateTempSubdirectory().FullName;
+                // 发布 APK
+                var args = $" publish \"{unoappcsproj}\"" +
+                                $" -f {AndroidTargetFramwork}" +
+                                $" -r {runtime}" +
+                                $" -c {Configuration}";
+                Run("dotnet" , args , rootPath);
 
-            var folder = Path.Combine(unorelease , runtime , "publish");
-            var apks = Directory.EnumerateFiles(folder , $"*-Signed.apk" , new EnumerationOptions() { RecurseSubdirectories = true });
-            var apk = apks.Single();
-            var newapk = Path.Combine(publishversionfolder,Path.GetFileNameWithoutExtension(apk) +$"-{runtime}.apk");
-            File.Move(apk , newapk , true);
-            Files.Add(newapk);
+                var folder = Path.Combine(unorelease , runtime , "publish");
+                var apks = Directory.EnumerateFiles(folder , $"*-Signed.apk" , new EnumerationOptions() { RecurseSubdirectories = true });
+                var apk = apks.Single();
+                var newapk = Path.Combine(publishversionfolder , Path.GetFileNameWithoutExtension(apk) + $"-{runtime}.apk");
+                File.Move(apk , newapk , true);
+                Files.Add(newapk);
+
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
     }
-    public void CleanBuildDirectories ()
+    [Obsolete]
+     void CleanBuildDirectories ()
     {
         string[] bin_obj = ["bin" , "obj"];
         string[] folders = ["WinApp" , "Database" , "Server" , "Core" , "UnoLibrary" , "UnoApp"];
-        foreach(var folder in folders)
+        foreach (var folder in folders)
         {
-            var directoryinfo=new DirectoryInfo(Path.Combine(rootPath,folder));
-        
-        foreach (var b_o in bin_obj)
+            var directoryinfo = new DirectoryInfo(Path.Combine(rootPath , folder));
+
+            foreach (var b_o in bin_obj)
             {
-            var objfolder = directoryinfo. GetDirectories(b_o).SingleOrDefault();
-            objfolder?.Delete(true);
+                var objfolder = directoryinfo.GetDirectories(b_o).SingleOrDefault();
+                objfolder?.Delete(true);
 
             }
 
