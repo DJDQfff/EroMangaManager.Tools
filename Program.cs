7600392502a80beb2a1d0f0ee2c3529a6d02cfa1
changbao zuo <2743109998@QQ.com>
2026-07-29 11:10:37 +0800

    zipmanga执行编码检测

--- Program.cs
+++ Program.cs
@@ -1,9 +1,14 @@
 ﻿global using static System.Console;
+using System.Text;
+using CommonLibrary;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using SharpCompress.Archives.Zip;
+using SharpCompress.Common;
+using SharpCompress.Providers.Default;
+using SharpCompress.Readers;
 using Tools;
 
-Console.WriteLine("Hello, World!");
-
-var version = "2026.7.26";
+var version = "2026.7.27";
 var slnfolder = "E:\\Projects\\EroMangaManager";
 
 var publisher = new GitHubReleasePublisher("DJDQfff", "EroMangaManager");
